ee099a27d3ae673ebd490c697100b5477545ad1d
milindoka <oak445@gmail.com>
2017-05-12 12:49:38 +0530

    Added KeyBoard Event Dispather For Global Uppercase Application

--- src/mrk/Main.java
+++ src/mrk/Main.java
@@ -1,11 +1,38 @@
 package mrk;
 
 
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
 import javax.swing.SwingUtilities;
 
 public class Main
 {
-    public static void main(String[] args) {           
+    public static void main(String[] args) 
+    {  
+    	
+    	/////////////////////////////////////KeyBoard Event Dispatcher ////////////
+    	
+    	
+    	  KeyboardFocusManager.getCurrentKeyboardFocusManager()
+          .addKeyEventDispatcher(new KeyEventDispatcher() {
+            public boolean dispatchKeyEvent(KeyEvent e) {
+              // This example converts all typed keys to upper
+              // case
+              if (e.getID() == KeyEvent.KEY_TYPED) {
+                e.setKeyChar(Character.toUpperCase(e
+                    .getKeyChar()));
+              }
+              // If the key should not be dispatched to the
+              // focused component, set discardEvent to true
+              boolean discardEvent = false;
+              return discardEvent;
+            }
+          });  	
+    	 
+    	/////////////////////////////////////KeyBoard Event Dispatcher //////////// 	  
+    	  
+    	  
         SwingUtilities.invokeLater(new Runnable()   
         {
             @Override
